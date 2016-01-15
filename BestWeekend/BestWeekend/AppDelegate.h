ea73bffc93d1249f9529dd431d2feb4fc99484cd
马娟娟 <2891529590@qq.com>
2016-01-15 18:08:39 +0800

    best

---
+++ BestWeekend/BestWeekend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BestWeekend
+//
+//  Created by scjy on 16/1/15.
+//  Copyright © 2016年 马娟娟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BestWeekend/BestWeekend/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BestWeekend
+//
+//  Created by scjy on 16/1/15.
+//  Copyright © 2016年 马娟娟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
